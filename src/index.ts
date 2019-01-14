03b0feeafee040e01ffd7545497bd5a4e5826218
Kameron <kameron.sween@rmn.com>
2019-01-14 10:47:44 -0600

    Fixing exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { Button } from './Button/Button.component';
+export { Avatar } from './Avatar';
+export { DropDown } from './DropDown';
+export { Container } from './Grid';
+export { Icon } from './Icon';
+export { Page, Footer, Header } from './Layout';
+export { List, Item as ListItem } from './List';
+export { Menu, Item as MenuItem } from './Menu';
+export { colors, fonts, sizes, variables, GlobalCSS, NormalizeCSS } from './theme';
