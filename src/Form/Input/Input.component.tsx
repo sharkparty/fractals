3ebd77b6af9e69816e7b028d346a7987b6011e58
Haowei Sun <hsun@rmn.com>
2019-01-25 16:23:35 -0600

    Add type and fix some styles

--- src/Form/Input/Input.component.tsx
+++ src/Form/Input/Input.component.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 // VENDOR
 import classNames from 'classnames';
 import styled from 'styled-components';
+import { get } from 'lodash';
 // THEME
-import { fonts, colors } from '../../theme';
+import { fonts, colors, variables } from '../../theme';
 
 interface InputProps {
   className?: string;
+  inputType?: any;
   label?: string;
   placeholder?: string;
   disabled?: boolean;
@@ -15,7 +17,6 @@ interface InputProps {
   value?: any;
   // onFocus?: () => void;
   // onBlur?: () => void;
-  onChange?: (e: any) => void;
   validate?: (e: any) => void;
   format?: (e: any) => void;
 
@@ -24,37 +25,48 @@ interface InputProps {
 }
 
 interface InputState {
-  isFocused: boolean;
+  isFocused?: boolean;
   value: string;
-  errorMessage: string;
+  errorMessage?: string;
 }
+
+const baseFontWeight = 500;
 
 export const StyledInput = styled.div `
   width: calc(100% - 2rem);
   height: 3rem;
   position: relative;
   display: flex;
-  flex-direction: column;
+  flex-flow: row wrap;
+  border-radius: ${variables.borderRadius};
+  border: solid 1px ${colors.neutrals.ash.light};
   font-family: ${fonts.fontFamily};
   opacity: ${({disabled}: InputProps) => disabled && '0.5'};
 
+  &:active,
+  &:focus {
+    outline: none;
+    border-color: ${({ disabled }: InputProps) => disabled ? colors.neutrals.ash.light : colors.neutrals.ash.dark};
+  }
+
+  &.error-border {
+    border-color: ${colors.alert.error.base};
+  }
+
   .field-input {
+    flex: 1;
+    overflow: hidden;
     width: 100%;
-    box-sizing : border-box;
-    padding: 24px 0 6px 0;
-    padding-left: ${({ prefix }: InputProps) => !prefix ? '.75rem' : 'calc(3rem - 3px)'};
-    border-radius: 2px;
-    border: solid 1px ${colors.neutrals.ash.light};
-    position: relative;
+    padding: 24px 0 6px ${({ prefix }: InputProps) => !prefix ? '.75rem' : 'calc(1rem - 3px)'};
     font-size: calc(1rem - 2px);
-    font-weight: 500;
+    font-weight: ${baseFontWeight};
     background: none;
     z-index: 1;
+    border: none;
 
     &:active,
     &:focus {
       outline: none;
-      border-color: ${({ disabled }: InputProps) => disabled ? colors.neutrals.ash.light : colors.neutrals.ash.dark};
     }
 
     &.error-border {
@@ -65,7 +77,8 @@ export const StyledInput = styled.div `
   .input-prefix {
     padding-top: 1rem;
     padding-left: 1rem;
-    position: absolute;
+    z-index: 2;
+    flex: 0 0 1rem;
   }
 
   .field-label {
@@ -74,8 +87,9 @@ export const StyledInput = styled.div `
     margin-top: calc(1rem - 1px);
     top: 0;
     font-size: calc(1rem - 2px);
-    font-weight: 500;
+    font-weight: ${baseFontWeight};
     line-height: 1.29;
+    /* TODO: kam figure out how to standardize transitions within elements! */
     transform-origin: 0% 50%;
     transition: all 200ms ease-in-out;
     color: ${colors.neutrals.charcoal.light};
@@ -96,23 +110,29 @@ export const StyledInput = styled.div `
   }
 
   .error {
+    // TODO: kam this is a bad idea... will reorganize everything
+    flex: 0 0 100%;
     margin-top: .5rem;
     font-size: .75rem;
-    font-weight: 500;
+    font-weight: ${baseFontWeight};
     color: ${colors.alert.error.base};
   }
 `;
 
 export class Input extends React.Component<InputProps> {
+  static defaultProps: InputProps = {
+    inputType: 'text',
+    format: (input?: any) => input,
+  };
+
   readonly state: InputState = {
-    isFocused: false,
     value: '',
-    errorMessage: '',
   };
 
   render(): React.ReactElement<Input> {
     const {
       label,
+      inputType,
       prefix,
       disabled,
     } = this.props;
@@ -132,6 +152,7 @@ export class Input extends React.Component<InputProps> {
           className: 'input-prefix',
         })}
         <input
+          type={inputType}
           disabled={disabled}
           onFocus={() => this.setState({ ...this.state, isFocused: true })}
           onBlur={() => this.setState({ ...this.state, isFocused: false })}
@@ -148,10 +169,12 @@ export class Input extends React.Component<InputProps> {
   }
 
   private handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    const errorMessage = this.props.validate && event.target.value && this.props.validate(event.target.value);
-    const value = this.props.format ? this.props.format(event.target.value) : event.target.value;
+    const value = get(event, 'target.value', '');
+
+    const errorMessage = this.props.validate && value && this.props.validate(value);
+    // TODO: consider removing strictNullChecks to enable defaultProps of functions
     this.setState({
-      value,
+      value: this.props.format ? this.props.format(value) : undefined,
       errorMessage,
     });
   }
